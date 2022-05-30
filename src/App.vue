fcb19ffa9618677985bc07c12b18204032d708b8
eduardo.bassan <eduardo.bassan@crieweb.com.br>
2022-05-30 13:47:51 -0300

    Commit init do projeto desafio vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MainApp>
+      <router-view />
+    </MainApp>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainApp from './components/MainApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainApp
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import './css/app.css';
+
+const routes = [
+    { 
+        path: '/', 
+        name: 'home', 
+        component: () => import('./components/pages/HomeApp.vue'),
+    },
+    { 
+        path: '/users', 
+        name: 'users', 
+        component: () => import('./components/pages/UsersApp.vue') 
+    },
+  ]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes, 
+    linkActiveClass: 'link-active',
+})
+
+createApp(App).use(router).mount('#app')
